179d6542da47877fe1bb382bc5ec7d7961aca943
taravind1995 <talariaravind090@gmail.com>
2023-09-05 19:24:42 +0530

    x 19

--- src/test/java/FrameWork/Selenium_Framework/EndToEnd.java
+++ src/test/java/FrameWork/Selenium_Framework/EndToEnd.java
@@ -12,47 +12,47 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class EndToEnd {
 	public static void main(String[] args) {
-		String uname="sam@123gmail.com";
-		String upassword="Sam@!123";
-		WebDriver driver=new ChromeDriver();
+		String uname = "sam@123gmail.com";
+		String upassword = "Sam@!123";
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/client");
-	
+
 		driver.findElement(By.cssSelector("#userEmail")).sendKeys(uname);
 		driver.findElement(By.cssSelector("#userPassword")).sendKeys(upassword);
 		driver.findElement(By.cssSelector("#login")).click();
-		
-		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
-		
-		
-	     List<WebElement> products=driver.findElements(By.cssSelector(".mb-3"));
-	     
-	
-			
-			  WebElement prod=products.stream().filter(product->
-			  product.findElement(By.cssSelector("b")).getText().equals("ADIDAS ORIGINAL")).
-			  findFirst().orElse(null);
-			 
-	
 
-	
-	 // wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".card-body button:last-of-type")));
-	  prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
-	 
-	/*
-	 * for(int i=0;i<products.size();i++) { String
-	 * product=products.get(i).getText();
-	 * 
-	 * if(product.contains("ADIDAS ORIGINAL")) {
-	 * driver.findElements(By.xpath("//div[@class='card-body']/button[2]")).get(i).
-	 * click();
-	 * 
-	 * //driver.findElements(By.cssSelector(".card-body button:last-of-type")).get(i
-	 * ).click(); } }
-	 */
-		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+
+		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+
+		WebElement prod = products.stream()
+				.filter(product -> product.findElement(By.cssSelector("b")).getText().equals("ADIDAS ORIGINAL"))
+				.findFirst().orElse(null);
+
+		// wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".card-body
+		// button:last-of-type")));
+		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector("[class*='toast-message']")));
+
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+
 	}
 
 }
+
+/*
+ * for(int i=0;i<products.size();i++) { String
+ * product=products.get(i).getText();
+ * 
+ * if(product.contains("ADIDAS ORIGINAL")) {
+ * driver.findElements(By.xpath("//div[@class='card-body']/button[2]")).get(i).
+ * click();
+ * 
+ * //driver.findElements(By.cssSelector(".card-body button:last-of-type")).get(i
+ * ).click(); } }
+ */
